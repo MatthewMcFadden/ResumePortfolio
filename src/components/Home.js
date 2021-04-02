85f42ec23edec8e920a7d2f3221e303523e5c814
Matt M <matt.mcfadden14@gmail.com>
2021-04-02 11:35:34 -0600

    finished homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,21 @@
 import React from "react"
+// import image from "../sky-background.jpg"
+import image from "../computer-coding.jpeg"
 
 export default function Home() {
-  return <h1> Home Page!</h1>
+  return (
+    <main>
+      <img 
+        src={image} 
+        alt="Sky Background" 
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        {/* dark:text isn't working */}
+        <h1 className="text-6xl text-white dark:text-white font-bold leading-none lg:leading-snug home-name">
+          Hi, I'm Matthew McFadden!
+        </h1>
+      </section>
+    </main>
+  )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,61 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+import { SocialIcon } from "react-social-icons"
 
 export default function NavBar() {
-  return <h1> NavBar Page!</h1>
+  return (
+    // Add class="navbar in header to work on fixed top nav"
+    <header className="bg-blue-800 dark:bg-gray-800">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink 
+            to="/" 
+            exact
+            activeClassName="text-white"
+              /* dark:text isn't working */
+            className="inflex-flex items-center py-6 px-3 mr-4 text-white dark:text-blue-700 hover:text-yellow-500 text-4xl font-bold tracking-widest"
+          >
+            McFadden
+          </NavLink>
+          <NavLink 
+            to="/post"
+            activeClassName="text-yellow-400 bg-blue-400"
+            className="inflex-flex items-center py-4 px-3 my-6 rounded text-blue-100 hover:text-yellow-500 font-bold"
+          >
+            Blog Posts
+          </NavLink>
+          <NavLink 
+            to="/project"
+            activeClassName="text-yellow-400 bg-blue-400"
+            className="inflex-flex items-center py-4 px-3 my-6 rounded text-blue-100 hover:text-yellow-500 font-bold"
+          >
+            Projects
+          </NavLink>
+          <NavLink 
+            to="/about"
+            activeClassName="text-yellow-400 bg-blue-400"
+            className="inflex-flex items-center py-4 px-3 my-6 rounded text-blue-100 hover:text-yellow-500 font-bold"
+          >
+            About Me!
+          </NavLink>
+        </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon 
+            url="https://www.linkedin.com/in/matthew-mcfadden-268aa71a6/" 
+            className="mr-4" 
+            target="_blank" 
+            fgColor="#fff" 
+            style={{height: 35, width: 35}}
+          />
+          <SocialIcon 
+            url="https://github.com/MatthewMcFadden" 
+            className="mr-4" 
+            target="_blank" 
+            fgColor="#fff" 
+            style={{height: 35, width: 35}}
+          />
+        </div>
+      </div>
+    </header>
+  )
 }
